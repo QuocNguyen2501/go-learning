43d5f198d5d2c0e434c647225a7aaabe6ffee3fc
Quoc Nguyen <quocngcntt@gmail.com>
2020-05-31 17:42:16 +0700

    restruct lessions

---
+++ go-fundametals/04-types/02-struct/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Customer struct {
+	Id        int
+	Name      string
+	Genre     string
+	Address   string
+	IsMarried bool
+}
+
+func main() {
+	customer := Customer{
+		Id:        1,
+		Name:      "Peter",
+		Genre:     "Male",
+		Address:   "Ajeltake Road, Ajeltake Island, Majuro, Marshall Islands",
+		IsMarried: true,
+	}
+
+	fmt.Println(customer)
+	fmt.Println("Id: ", customer.Id)
+	fmt.Println("Name: ", customer.Name)
+	fmt.Println("Genre: ", customer.Genre)
+	fmt.Println("Address: ", customer.Address)
+	fmt.Println("IsMarried: ", customer.IsMarried)
+}
